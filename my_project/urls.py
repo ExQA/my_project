bc77e72b5c4c472e32238afcf5fc12914abe980d
denissv <volshebnik00@gmail.com>
2019-10-19 21:38:52 +0300

    added header

--- my_project/urls.py
+++ my_project/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('calculator/', views.calculator)
+    path('calculator/', views.calculator, name='calculator')
 
 ]
